8991fb19b5a7398fe56e5beb9bf88d632b4e8316
ncflsy <flassynico@gmail.com>
2023-09-19 10:09:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tokped/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.tokped;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView navigationView = findViewById(R.id.navbar);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        if (item.getItemId() == R.id.fr_beranda) {
+            fragment = new HomeFragment();
+        } else if (item.getItemId() == R.id.fr_keranjang) {
+            fragment = new CartFragment();
+        } else if (item.getItemId() == R.id.fr_notifikasi) {
+            fragment = new NotifFragment();
+        } else if (item.getItemId() == R.id.fr_akun) {
+            fragment = new AccFragment();
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
